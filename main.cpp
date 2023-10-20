cf4ffbae5566d34f8d2472b81ffec14a15a5f2f3
Panos Panagiotopoulos <panospanos570@gmail.com>
2023-10-20 16:57:35 +0300

    Prosthesh basikou kwdika apo ekfwnhsh kai structure

--- main.cpp
+++ main.cpp
@@ -3,9 +3,34 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+extern int yylex(FILE* fpointer, union semantic_info* sem);
+
+extern union semantic_info {
+	char* s;
+	int i;
+} SEMANTIC_INFO;
+
 // main of program
 void main() {
+	FILE *fp = NULL;
+	// Return values come here
+	union semantic_info sem;
 
+	// Open file
+	fopen_s(&fp, "data.txt", "r");
+
+	int tokentype = 0;
+
+
+	while ((tokentype = yylex(fp, &sem)) != 0) {
+		if (tokentype == 1) {
+			// Action 1
+		}
+
+		else if (tokentype == 2) {
+			// Action 2
+		}
+	}
 
 
 
--- yylex.cpp
+++ yylex.cpp
@@ -3,13 +3,51 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+union semantic_info {
+	char* s;
+	int i;
+} SEMANTIC_INFO;
 
 // yylex Function gia anagnwrish lexewn
-int yylex() {
-	char yylex[100] = {0};
-	// This is Panos writting code
+int yylex(FILE *fpointer, union semantic_info *sem ) {
+	char lexeme[100] = {0};
+	int index = 0;
+	int state = 0;
 
 
+	while ((lexeme[index] = fgetc(fpointer)) != EOF) {
+		switch (state) {
+		// State 0
+		case 0: {
+			if (isalpha(lexeme[index])) {
+
+			}
+
+			else if (isdigit(lexeme[index])) {
+
+			}
+
+			else {
+
+			}
+
+			break;
+		}
+
+		// State 1
+		case 1: {
+
+			break;
+		}
+
+		// State 2
+		case 2: {
+
+			break;
+		}
+
+		}
+	}
 
 	return 1;
 }
