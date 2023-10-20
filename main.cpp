4cb9f99735d03895c5a9ac3341f8715dc23ffe97
Panos Panagiotopoulos <panospanos570@gmail.com>
2023-10-20 15:23:54 +0300

    Initialaize Commit. Start of exercise

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// main of program
+void main() {
+
+
+
+
+
+
+
+}
---
+++ yylex.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+// yylex Function gia anagnwrish lexewn
+int yylex() {
+
+
+
+
+
+	return 1;
+}
